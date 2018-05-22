fc98caa2e8e78837df5fc565f56079b2eb96a9f1
Saugat Sthapit <saugat.sthapit@gmail.com>
2018-05-22 14:34:34 -0400

    Updated

--- newpy1.py
+++ newpy1.py
@@ -11,7 +11,7 @@ sys.path.append(PLASMAROOT)
 sys.path.append(ALPHAROOT)
 sys.path.append(PLASMAROOT + "/bin_27")
 sys.path.append(PYTHONROOT)
-sys.path.append(ALPHAROOT + "bin")
+sys.path.append(ALPHAROOT + "/bin")
 sys.path.append(ALPHAROOT + "/extern/bin")
 sys.path.append(PROGRAMROOT + "/Plasma/extern/bin")
 sys.path.append(PROGRAMROOT + "/Plasma/extern/bin/python")
@@ -27,6 +27,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import mysql.connector
 from mysql.connector import errorcode
+import json
+import base64
 import untitled
 import dicom
 import imagetools
@@ -43,7 +45,9 @@ import subprocess
 import plasma
 import time
 import win32gui
-
+import ivtools
+import random
+import string
 __metaclass__ = type
 
 try:
@@ -64,8 +68,9 @@ except mysql.connector.Error as err:
         print(err)
 
 class ExampleApp(QDialog):
-    cell = 0
+    IMAGE = 0
     windowlevel= []
+
 
     def __init__(self):
         # super allows us to
@@ -76,15 +81,11 @@ class ExampleApp(QDialog):
                             # It sets up layout and widgets that are defined
         self.setWindowState(Qt.WindowMaximized)
         self.ui.btn1.clicked.connect(self.insertList)
-        self.ui.btn2.clicked.connect(self.loadData)
+        self.ui.btn2.clicked.connect(self.loadDicomInfo)
         self.ui.tableWidget.cellDoubleClicked.connect(self.loadImage)
+        self.ui.tableWidget_2.cellDoubleClicked.connect(self.showAnnotation)
         self.ui.btnR.clicked.connect(self.saveAnnotation)
-        # exePath = "C:\Users\z003x7ma\source\repos\FlaskWebProject1\FlaskWebProject1\FlaskWebProject1\cad\Plasma\bin_27\Plasma.exe"
-        # subprocess.Popen(exePath)
-        # hwnd = win32gui.FindWindowEx(0, 0, "PlasmaFrame", "1")
-        # time.sleep(0.05)
-        # window = QWindow.fromWindId(hwnd)
-        # self.createWindowContainer(window, self)
+        self.ui.btnC.clicked.connect(self.loadAnnotationInfo)
         self.show()
 
     def insertList(self):
@@ -94,45 +95,76 @@ class ExampleApp(QDialog):
         rows = cursor.fetchone()
         print(rows)
 
-    def loadData(self):
+    def loadDicomInfo(self):
         query = ('select * from volume_dicominfo');
+        tableWidget = self.ui.tableWidget
+        self.loadData(query, tableWidget)
+
+    def loadAnnotationInfo(self):
+        loadquery = ('select * from windowlevel_annotation');
+        tableWidget = self.ui.tableWidget_2
+        self.loadData(loadquery, tableWidget)
+
+    def loadData(self, query, tableWidgetName):
         cursor.execute(query)
         result = cursor.fetchall()
-        self.ui.tableWidget.setRowCount(0)
+        tableWidgetName.setRowCount(0)
         for row_number, row_data in enumerate(result):
-            self.ui.tableWidget.insertRow(row_number)
+            tableWidgetName.insertRow(row_number)
             for column_number, data in enumerate(row_data):
-                self.ui.tableWidget.setItem(row_number, column_number, QTableWidgetItem(str(data)))
-                print("This is column ", column_number)
-                print("This is row ", row_number)
+                tableWidgetName.setItem(row_number, column_number, QTableWidgetItem(str(data)))
 
     def loadImage(self, row, column_number):
-        global cell
-        cell = self.ui.tableWidget.item(row, column_number).text()
-        print ("CELL VALUE FROM LOAD IMAGE: ", cell)
+        global IMAGE
+        IMAGE = self.ui.tableWidget.item(row, column_number).text()
+        print ("CELL VALUE FROM LOAD IMAGE: ", IMAGE)
         plasma.die()
         plasma.start()
-        viewertools.show(cell)
+        viewertools.show(IMAGE)
 
     def saveAnnotation(self):
-        # Get the ROI size
-        print ("Roi size is ", float(vpiv.getfov(0)))
-        # Get the cursor center
-        # print(ivtools.currentworldcord(vpiv.currentimage()))
-        # Get the window levels
-        print("Window levels ", cad_utils.floatlist(vpiv.getscalarwindow(plasmatools.allsids()[0])))
-        # set the window levels
-        Windowlevel = [200, 1700]
-        print ("CELL IS ", cell)
-        img = dicomloader.load(cell, "img")
-        vpiv.setscalarwindow(img, windowlevel[0], windowlevel[1], 1)
+        uid = ''.join(random.Random().sample(string.letters + string.digits, random.randint(10,20)))
+        world_FovSize = float(float(vpiv.getfov(vpiv.currentimage()))) #size of ROI
+        world_cord = cad_utils.floatlist(vpiv.currentworldcoordinateswithwindowid(vpiv.currentimage())) #world location
+        world_cord_str = str(world_cord)
+        window_low, window_high = cad_utils.floatlist(vpiv.getscalarwindow(plasmatools.allsids()[0]))
+        b0= ivtools.getBookmarkedView() #complete details
+        strb0 = ' '.join(b0)
+        insertQuery = """ INSERT INTO windowlevel_annotation(anno_id, world_coordination, world_dimension, window_low, window_high, annotation_details)
+        VALUES(%s,%s,%s,%s,%s,%s) """
+        cursor.execute(insertQuery,(uid, world_cord_str, world_FovSize, window_low, window_high, strb0))
+        connection.commit()
 
+    def showAnnotation(self, row, column_number):
+        print ("Row: %d and Column is %d" %(row, column_number))
+        annoDetailsColumn = 5
+        windowHighColumn = 4
+        windowLowColumn = 3
+        worldCoordColumn = 1
+        if not column_number == 5:
+            temp_column_number = 5 - column_number #actual column_number
+            annoDetailsColumn = temp_column_number + column_number #always 5
+            print (column_number)
+        annoDetailCell = self.ui.tableWidget_2.item(row,column_number).text()
+        print (annoDetailCell)
+        
+        vpiv.goto(world_cord[0], world_cord[1], world_cord[2])
+        vpiv.setfov(world_FovSize)
+        vpiv.setscalarwindow(img, window_low, window_high, 1)
+
+
+    # def ShowAnnotation(world_FovSize, window_low, window_high, world_cord, cell):
+    #     vpiv.goto(world_cord[0], world_cord[1], world_cord[2])
+    #     vpiv.setfov(world_FovSize)
+    #     vpiv.setscalarwindow(img, window_low, window_high, 1)
 
 def main():
     app = QApplication(sys.argv)  # A new instance of QApplication
     form = ExampleApp()                 # We set the form to be our ExampleApp (design)
     form.setWindowFlags(Qt.Window | Qt.FramelessWindowHint)
     form.move(0,0)
+    # import pdb
+    # pdb.set_trace()
     form.show()                         # Show the form
     sys.exit(app.exec_())                      # and execute the app
 
